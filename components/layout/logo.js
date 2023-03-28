c25d8344980d7f424c3f0cb514d18c387f9ce1cd
Samuel <timeuss.craft@gmail.com>
2023-03-28 23:00:31 +0200

    add layout & navbar

---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import classes from './logo.module.css'
+
+const Logo = () => {
+  return <div className={classes.logo}>Sam's NextJs Blog</div>
+}
+
+export default Logo
