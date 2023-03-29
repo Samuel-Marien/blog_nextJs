5ba6fb933d0c9692bee73a54d2b29d844cb41bbc
Samuel <timeuss.craft@gmail.com>
2023-03-29 14:17:20 +0200

    add all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,44 @@
 import React from 'react'
 
+import AllPosts from '../../components/posts/all-posts'
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextJs',
+    title: 'getting started with nextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextJs2',
+    title: 'getting started with reactjs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextJs3',
+    title: 'getting started with nextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextJs4',
+    title: 'getting started with reactjs2',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+    date: '2022-02-10'
+  }
+]
+
 const AllPostsPage = () => {
-  return <div>AllPostsPage</div>
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage
