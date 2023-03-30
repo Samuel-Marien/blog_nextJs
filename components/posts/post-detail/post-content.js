92b6637c7500f4cdc500b93e8e475259cc7d3203
Samuel <timeuss.craft@gmail.com>
2023-03-30 10:38:02 +0200

    rendering dynamic post page & paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,23 +4,24 @@ import ReactMarkdown from 'react-markdown'
 import classes from './post-content.module.css'
 import PostHeader from './post-header'
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextJs',
-  title: 'getting started with nextJs',
-  image: 'getting-started-nextjs.png',
-  excerpt:
-    "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
-  date: '2022-02-10',
-  content: '# This is a first post'
-}
+// const DUMMY_POST = {
+//   slug: 'getting-started-with-nextJs',
+//   title: 'getting started with nextJs',
+//   image: 'getting-started-nextjs.png',
+//   excerpt:
+//     "The React Framework for the Web Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.",
+//   date: '2022-02-10',
+//   content: '# This is a first post'
+// }
 
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+const PostContent = (props) => {
+  const { post } = props
+  const imagePath = `/images/posts/${post.slug}/${post.image}`
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
 
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 import PostContent from '../../components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '../../lib/posts-utils'
 
-const PostDetailPage = () => {
-  return <PostContent />
+const PostDetailPage = (props) => {
+  return <PostContent post={props.post} />
+}
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const { slug } = params
+
+  const postData = getPostData(slug)
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 180
+  }
+}
+
+export async function getStaticPaths() {
+  const postFilenames = getPostsFiles()
+  console.log(postFilenames)
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ''))
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false
+  }
 }
 
 export default PostDetailPage
